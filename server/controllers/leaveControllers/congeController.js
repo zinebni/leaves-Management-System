cda162e73d8bb13e478adb877b97ef4d06b8e462
zineb bennani <140564571+zinebni@users.noreply.github.com>
2025-07-17 16:28:49 +0100

    conge routes

--- server/controllers/leaveControllers/congeController.js
+++ server/controllers/leaveControllers/congeController.js
@@ -1,12 +1,12 @@
 import dayjs from "dayjs";
 import Conge from "../../models/congeModel.js";
 import DroitConge from "../../models/droitCongeModel.js";
+import mongoose from "mongoose";
 //Employé---------------------------------------------------------Employé :
 
 // createLeaveRequest() 
 export const createLeaveRequest = async (req, res) => {
   const  employeeId  = req.user.id;
-  const organisation = req.user.organisation;
   const { date_debut, date_fin, motif, commentaire} = req.body;
   const justificatifs = req.files ? req.files.map(f => f.filename) : [];  //passer par multer
 
@@ -19,7 +19,7 @@ export const createLeaveRequest = async (req, res) => {
 
   const nombreDeJoursDemande=  dayjs(date_fin).diff(dayjs(date_debut), 'day') + 1; //dayjs(date_fin).diff(dayjs(date_debut), 'day') Cette expression calcule la différence en jours entiers entre les deux dates, en excluant la date de début pour cela on ajoute +1
   
-  const droitConge = await DroitConge.findOne({_id:motif, employee: employeeId, organisation : organisation});
+  const droitConge = await DroitConge.findOne({_id:motif, employee: employeeId});
   if (!droitConge) {
     return res.status(400).json({ message: "Droit de congé introuvable." });
   }
@@ -112,8 +112,213 @@ export const getMyLeaveRequestsByStatus = async (req, res) => {
   }
 };
 //RH------------------------------------------------------RH :
-    // getAllLeaveRequests() 
-    // getLeaveRequestById(id)
-    // approveLeaveRequest(id) 
-    // rejectLeaveRequest(id) 
+// getAllLeaveRequests() 
+export const getAllLeaveRequests = async (req, res) => {
+  const organisationId =  new mongoose.Types.ObjectId(req.user.organisation);
+  if(!organisationId){
+    return res.status(400).json({ success: false, message: 'organisation non trouvée.' });
+  }
+  try {
+    const conges = await Conge.aggregate([
+        {
+          $lookup: {
+            from: 'employees', 
+            localField: 'employee',
+            foreignField: '_id',
+            as: 'employee'
+          }
+        },
+        { $unwind: '$employee' },
+        {
+          $match: {
+            'employee.organisation': organisationId
+          }
+        },
+        {
+          $project: {
+            createdAt: 0,
+            updatedAt: 0,
+            __v: 0
+          }
+        }
+      ]) 
+    res.status(200).json({ success: true, conges , count : conges.length});
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+
+// getLeaveRequestById(id conge)
+export const getLeaveRequestById = async (req, res) => {
+  const { id } = req.params;
+  const organisation = req.user.organisation;
+  try {
+    const conge = await Conge.findOne({ _id: id}, {createdAt:0, updatedAt:0, __v:0, deleted:0}).populate({path: 'motif', select: 'type'}).populate({path: 'employee', select: 'nom prenom department'});
+    if (!conge) {
+      return res.status(404).json({ success: false, message: 'Demande de congé non trouvée.' });
+    }
+    res.status(200).json({ success: true, conge });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+//get all leave requests by status
+export const getAllLeaveRequestsByStatus = async (req, res) => {
+  const { status } = req.params;
+  const organisationId =  new mongoose.Types.ObjectId(req.user.organisation);
+  if(!organisationId){
+    return res.status(400).json({ success: false, message: 'organisation non trouvée.' });
+  }
+  try {
+    const conges = await Conge.aggregate([
+        {
+          $lookup: {
+            from: 'employees', 
+            localField: 'employee',
+            foreignField: '_id',
+            as: 'employee'
+          }
+        },
+        { $unwind: '$employee' },
+        {
+          $match: {
+            'employee.organisation': organisationId,
+             status: status
+          }
+        },
+        {
+          $project: {
+            createdAt: 0,
+            updatedAt: 0,
+            __v: 0
+          }
+        }
+      ]) 
+    res.status(200).json({ success: true, conges , count : conges.length});
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+  
+};
+
+//get all leave requests by employee
+export const getAllLeaveRequestsByEmployee = async (req, res) => {
+  const { employeeId } = req.params;
+  const organisationId =  new mongoose.Types.ObjectId(req.user.organisation);
+  if(!organisationId){
+    return res.status(400).json({ success: false, message: 'organisation non trouvée.' });
+  }
+  try {
+    const conges = await Conge.aggregate([
+        {
+          $lookup: {
+            from: 'employees', 
+            localField: 'employee',
+            foreignField: '_id',
+            as: 'employee'
+          }
+        },
+        { $unwind: '$employee' },
+        {
+          $match: {
+            'employee.organisation': organisationId,
+            'employee._id': new mongoose.Types.ObjectId(employeeId)
+          }
+        },
+        {
+          $project: {
+            createdAt: 0,
+            updatedAt: 0,
+            __v: 0
+          }
+        }
+      ]) 
+    res.status(200).json({ success: true, conges , count : conges.length});
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+  
+};  
+
+//get all leave requests by department
+export const getAllLeaveRequestsByDepartment = async (req, res) => {
+  const { departmentId } = req.params;
+  const organisationId =  new mongoose.Types.ObjectId(req.user.organisation);
+  if(!organisationId){
+    return res.status(400).json({ success: false, message: 'organisation non trouvée.' });
+  }
+  try {
+    const conges = await Conge.aggregate([
+        {
+          $lookup: {
+            from: 'employees',
+            localField: 'employee',
+            foreignField: '_id',
+            as: 'employee'
+          }
+        },
+        { $unwind: '$employee' },
+        {
+          $match: {
+            'employee.organisation': organisationId,
+            'employee.department': new mongoose.Types.ObjectId(departmentId)
+          }
+        },
+        {
+          $project: {
+            createdAt: 0,
+            updatedAt: 0,
+            __v: 0
+          }
+        }
+      ])
+    res.status(200).json({ success: true, conges, count : conges.length});
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+
+};
+
+//get all leave requests by department and status
+export const getAllLeaveRequestsByDepartmentAndStatus = async (req, res) => {
+  const { departmentId, status } = req.params;
+  const organisationId =  new mongoose.Types.ObjectId(req.user.organisation);
+  if(!organisationId){
+    return res.status(400).json({ success: false, message: 'organisation non trouvée.' });
+  }
+  try {
+    const conges = await Conge.aggregate([
+        {
+          $lookup: {
+            from: 'employees',
+            localField: 'employee',
+            foreignField: '_id',
+            as: 'employee'
+          }
+        },
+        { $unwind: '$employee' },
+        {
+          $match: {
+            'employee.organisation': organisationId,
+            'employee.department': new mongoose.Types.ObjectId(departmentId),
+            status: status
+          }
+        },
+        {
+          $project: {
+            createdAt: 0,
+            updatedAt: 0,
+            __v: 0
+          }
+        }
+      ])
+    res.status(200).json({ success: true, conges, count : conges.length});
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+
+};
+// approveLeaveRequest(id) 
+// rejectLeaveRequest(id) 
 
--- server/routes/conge/cogeRoutes.js
+++ server/routes/conge/cogeRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import upload from '../../middleware/upload.js';
-import { cancelLeaveRequest, createLeaveRequest , getMyLeaveRequests, getMyLeaveRequestsByStatus, getMyLeaveRequestsDeleted} from '../../controllers/leaveControllers/congeController.js';
+import { cancelLeaveRequest, createLeaveRequest , getAllLeaveRequests, getAllLeaveRequestsByDepartment, getAllLeaveRequestsByDepartmentAndStatus, getAllLeaveRequestsByEmployee, getAllLeaveRequestsByStatus, getLeaveRequestById, getMyLeaveRequests, getMyLeaveRequestsByStatus, getMyLeaveRequestsDeleted} from '../../controllers/leaveControllers/congeController.js';
 import authMiddleware from '../../middleware/auth.js';
+import isRhMiddleware from '../../middleware/isRh.js';
 
 //1- utiliser express pour creer un routeur 
 const congeRouter = express.Router(); 
@@ -17,6 +18,12 @@ congeRouter.delete('/cancelLeaveRequest/:id',authMiddleware,cancelLeaveRequest);
 
 
 //routes for RH
+congeRouter.get('/getAllLeaveRequests',authMiddleware,isRhMiddleware,getAllLeaveRequests);
+congeRouter.get('/getAllLeaveRequestsByStatus/:status',authMiddleware,isRhMiddleware,getAllLeaveRequestsByStatus);
+congeRouter.get('/getLeaveRequestById/:id',authMiddleware,isRhMiddleware,getLeaveRequestById);
+congeRouter.get('/getAllLeaveRequestsByEmployee/:employeeId',authMiddleware,isRhMiddleware,getAllLeaveRequestsByEmployee);
+congeRouter.get('/getAllLeaveRequestsByDepartment/:departmentId',authMiddleware,isRhMiddleware,getAllLeaveRequestsByDepartment);
+congeRouter.get('/getAllLeaveRequestsByDepartmentAndStatus/:departmentId/:status',authMiddleware,isRhMiddleware,getAllLeaveRequestsByDepartmentAndStatus);
 
 //3-exportation du routeur
 export default congeRouter;
